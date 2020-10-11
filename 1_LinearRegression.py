537ae4fcfc8b2ed1377daa9f43a217a0105fb842
Raju Maramulla <raju.maramulla@gmail.com>
2020-10-11 13:14:15 +0530

    Update 1_LinearRegression.py

--- 1_LinearRegression.py
+++ 1_LinearRegression.py
@@ -5,27 +5,26 @@ import matplotlib.pyplot as plt
 
 #Download the dataset from Kaggle
 #https://www.kaggle.com/andonians/random-linear-regression/data
-file_path = r'C:\Users\User\Downloads\archive\train.csv'
+train_path = r'C:\Users\User\Downloads\archive\train.csv'
+test_path = r'C:\Users\User\Downloads\archive\test.csv'
 
 #Read the dataset
-data = pd.read_csv(file_path)
+train = pd.read_csv(train_path)
+test = pd.read_csv(test_path)
 
 #Clean dataset for null values(if any)
-data = data.dropna()
+train = train.dropna()
 
 #Set Hyperparameter values
-iterations = 10
+iterations = 20
 learning_rate = 0.0001
 
-#Split the dataset into train and test sets
-test_ratio = 0.3
-split_index = data.shape[0] - int(data.shape[0] * test_ratio)
+#Store data and labels separately
+train_data = np.array(train.x).reshape(train.shape[0], 1)
+train_labels = np.array(train.y).reshape(train.shape[0], 1)
 
-train_data = np.array(data.x[0:split_index]).reshape(split_index, 1)
-train_labels = np.array(data.y[0:split_index]).reshape(split_index, 1)
-
-test_data = np.array(data.x[split_index:]).reshape(data.shape[0] - split_index, 1)
-test_labels = np.array(data.y[split_index:]).reshape(data.shape[0] - split_index, 1)
+test_data = np.array(test.x).reshape(test.shape[0], 1)
+test_labels = np.array(test.y).reshape(test.shape[0], 1)
 
 print("TrainData Dimensions: X: {} Y: {} \nTestData Dimensions: X: {} Y: {} \n"
       .format(train_data.shape, train_labels.shape, test_data.shape, test_labels.shape))
@@ -46,8 +45,8 @@ for iteration in range(iterations):
     print("Iteration: {}, Loss: {}".format(iteration+1, cost))
     
     #Plot the current status
-    plt.plot(train_data, train_labels, 'x')
-    plt.plot(train_data, predictions, 'o')
+    plt.plot(train_data, train_labels, '.')
+    plt.plot(train_data, predictions, linewidth=2)
     plt.show()
     
     #Gradient Descent for back propagation
@@ -65,6 +64,6 @@ cost_test = np.mean((test_labels - test_predictions) ** 2) * 0.5
 print("Model performance on test data \nCost: {}".format(cost_test))
 
 plt.figure()
-plt.plot(test_data, test_labels, 'x')
-plt.plot(test_data, test_predictions, 'o')
+plt.plot(test_data, test_labels, '.')
+plt.plot(test_data, test_predictions, linewidth=2)
 plt.show()
